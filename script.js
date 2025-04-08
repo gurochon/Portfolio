aa10c16d57e29a704da94a7d1e6338fe7e71448f
gurochon <153224022+gurochon@users.noreply.github.com>
2025-04-08 18:19:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-
     // Modal - Ouvrir l'image du CV au clic
     document.getElementById('cvImage').onclick = function() {
         document.getElementById('modalImage').src = this.src;
@@ -7,32 +6,41 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Fermer la modal au clic sur le bouton de fermeture
-    document.getElementsByClassName('close')[0].onclick = function() {
-        document.getElementById('cvModal').style.display = "none";
-    }
+    document.querySelectorAll('.close').forEach(closeButton => {
+        closeButton.onclick = function() {
+            document.getElementById('cvModal').style.display = "none";
+            document.getElementById('imageModal').style.display = "none";
+        }
+    });
+
+    // Ouvrir le modal d'image au clic sur "Voir plus"
+    document.querySelectorAll('.voir-plus').forEach(button => {
+        button.onclick = function() {
+            const imageSrc = this.getAttribute('data-image');
+            document.getElementById('imageModalContent').src = imageSrc;
+            document.getElementById('imageModal').style.display = "block";
+        }
+    });
 
     // Fonction de défilement fluide vers chaque section
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
 
-            const targetId = this.getAttribute('href').substring(1); // Récupère l'ID de la section cible
-            const targetElement = document.getElementById(targetId); // Trouve l'élément cible
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
 
-            // Si c'est la section "Accueil", on scroll tout en haut de la page
             if (targetId === "accueil") {
                 window.scrollTo({
-                    top: 0, // Position en haut de la page
-                    behavior: 'smooth' // Effet de défilement fluide
+                    top: 0,
+                    behavior: 'smooth'
                 });
             } else {
-                // Défilement fluide vers les autres sections
                 window.scrollTo({
-                    top: targetElement.offsetTop, // Position de la section cible
-                    behavior: 'smooth' // Effet de défilement fluide
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
                 });
             }
         });
     });
-
 });
